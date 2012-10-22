70dfd10e33f562ef709794797a831dae12df5a79
Jairo Andrés Valencia <jaanvagu@hotmail.com>
2012-10-21 22:40:14 -0500

    Actualización de Main

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,40 @@
  */
 
 
+import java.util.Vector;
+
 public class Main {
 
     private static LeerArchivoCSV leerArchivoCSV;
     private static Preprocesamiento preprocesamiento;
-    private static Lematizar lematizar;
+    private static Lematizar lematizar;    
+
+    //Variable que almacena los comentarios de la siguiente manera:
+    //  Lista de Comentarios Normalizados = [ComentarioNormalizado(1), ComentarioNormalizado(2), ..., ComentarioNormalizado(N)  ]
+    //  Donde ComentarioNormalizado(i):
+    //      Lista de palabras del comentario = [palabra1, palabra2, ..., palabraN]
+    //      Etiquetas = [etiqueta1, etiqueta2, ..., etiquetaN]
+    public static Vector<ComentarioNormalizado> listaComentariosNormalizados;
+
+    //Método que construye la lista de comentarios normalizados, a partir de los comentarios preprocesados y lematizados,
+    //y las etiquetas relacionadas a cada comentario. 
+    private static void normalizarComentarios(Vector<String> listaComentariosPreprocesadosYLematizados,
+                                              Vector<Comentario> listaComentariosOriginal){
+        listaComentariosNormalizados = new Vector();
+        for(int i=0; i<listaComentariosOriginal.size(); i++){
+            ComentarioNormalizado comentarioNormalizadoActual;
+            comentarioNormalizadoActual = new ComentarioNormalizado(listaComentariosPreprocesadosYLematizados.elementAt(i),
+                                                                    listaComentariosOriginal.elementAt(i).obtenerEtiquetas());
+            listaComentariosNormalizados.addElement(comentarioNormalizadoActual);
+        }
+    }
 
     public static void main(String[] args) {
         
         leerArchivoCSV = new LeerArchivoCSV();
         leerArchivoCSV.leerYAlmacenarLineasCSV();
         preprocesamiento = new Preprocesamiento(leerArchivoCSV.obtenerListaComentariosLeidos());
-        lematizar = new Lematizar(preprocesamiento.obtenerMensajesProcesados());        
+        lematizar = new Lematizar(preprocesamiento.obtenerMensajesProcesados());
+        normalizarComentarios(lematizar.obtenerListaMensajesLematizados(), leerArchivoCSV.obtenerListaComentariosLeidos());        
     }
 }
