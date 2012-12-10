75326f6ecc29c00d76c2e37b2dad2521ffa24749
Jairo Andrés Valencia <jaanvagu@gmail.com>
2012-12-10 13:32:53 -0500

    Clase GestionarVectorPalabras añadida
    Clase encargada de generar vector n-dimensional con las diferentes
    palabras de una lista de comentarios. Además, encargada de generar los
    vectores para cada comentario con las frecuencias de las palabras en él.

---
+++ src/GestionarVectorPalabras.java
@@ -0,0 +1,97 @@
+/*
+ * @author Jairo Andrés
+ */
+
+
+import java.util.*;
+
+public class GestionarVectorPalabras {
+
+    private Vector<ComentarioNormalizado> listaComentariosNormalizados;
+    private GestionarArchivos gestionArchivos;
+
+    private Vector<String> vectorPalabras;
+    private Vector<VectorFrecuenciasPalabras> listaVectoresDeFrecuencias;
+
+    //Constructor que lee la lista de comentarios normalizados desde un archivo local
+    public GestionarVectorPalabras(){
+        vectorPalabras = new Vector();
+        listaVectoresDeFrecuencias = new Vector();
+        gestionArchivos = new GestionarArchivos();
+        //listaComentariosNormalizados = gestionArchivos.cargarComentariosNormalizados("C:/Users/Jairo Andrés/Desktop/Archivos Guardados/Prueba - Normalizado");
+        listaComentariosNormalizados = gestionArchivos.cargarComentariosNormalizados(
+                "C:/Users/Jairo Andrés/Desktop/Archivos Guardados/P2 - Normalizado");
+    }
+
+    //Constructor que recibe como parametro la lista de comentarios normalizados.
+    public GestionarVectorPalabras(Vector<ComentarioNormalizado> listaComentariosNormalizados){
+        vectorPalabras = new Vector();
+        listaVectoresDeFrecuencias = new Vector();
+        this.listaComentariosNormalizados = listaComentariosNormalizados;
+    }
+
+    //Contruye un vector de palabras, con todas las palabras diferentes contenidas en una lista de comentarios.
+    //La ultima posicion contiene la frecuencia de los comentarios vacios (despues de realizartles preprocesamiento quedaron vacios).
+    public void contruirVectorDePalabras(){
+        for(int i=0; i<listaComentariosNormalizados.size(); i++){
+            for(int j=0; j<listaComentariosNormalizados.elementAt(i).obtenerListaPalabrasEnComentario().size(); j++){
+                String palabra =  listaComentariosNormalizados.elementAt(i).obtenerListaPalabrasEnComentario().elementAt(j);
+                if(!palabraEstaGuardada(palabra))
+                    vectorPalabras.addElement(palabra);
+            }            
+        }
+        vectorPalabras.addElement("ComentarioVacio");
+    }
+
+    //Crea un vector de frecuencias de palabras para cada uno de los comentarios de la lista de comentarios normalizados
+    //de acuerdo al vector de palabras. En otras palabras, se cuentan las ocurrencias de las palabras en cada comentario.
+    //Ejemplo:
+    //Comentario 1 : [palabra1, palabra2]
+    //Comentario 2 : [palabra3, palabra4]
+    //Comentario 3 : [palabra1, palabra3]
+    //Vector de palabras:                 [palabra1, palabra2, palabra3, palabra4]
+    //Vector de frecuencias Comentario 1: [   1    ,    1    ,    0    ,    0    ]
+    //Vector de frecuencias Comentario 2: [   0    ,    0    ,    1    ,    1    ]
+    //Vector de frecuencias Comentario 3: [   1    ,    0    ,    1    ,    0    ]
+    public void generarVectoresDeFrecuenciasDePalabras(){
+        for(int i=0; i<listaComentariosNormalizados.size(); i++){
+            VectorFrecuenciasPalabras vectorFrecuenciasPalabras = new VectorFrecuenciasPalabras(vectorPalabras.size());
+            for(int j=0; j<listaComentariosNormalizados.elementAt(i).obtenerListaPalabrasEnComentario().size(); j++){
+                String palabra =  listaComentariosNormalizados.elementAt(i).obtenerListaPalabrasEnComentario().elementAt(j);
+                int posicionPalabra = vectorPalabras.indexOf(palabra);                
+                vectorFrecuenciasPalabras.aumentarFrecuenciaEnPosicion(posicionPalabra);
+            }
+            if(esComentarioVacio(listaComentariosNormalizados.elementAt(i)))
+                vectorFrecuenciasPalabras.aumentarFrecuenciaEnPosicionDeComentarioVacio();
+            listaVectoresDeFrecuencias.add(vectorFrecuenciasPalabras);
+        }
+    }
+
+    //Verifica si la plabra ya ha sido almacenada en el vector de palabras.
+    public boolean palabraEstaGuardada(String palabra){
+        if(vectorPalabras.contains(palabra))
+            return true;
+        else
+            return false;
+    }
+
+    public boolean esComentarioVacio(ComentarioNormalizado comentario){
+        if(comentario.obtenerListaPalabrasEnComentario().isEmpty())
+            return true;
+        else
+            return false;
+    }
+
+    public Vector<ComentarioNormalizado> obtenerListaComentariosNormalizados(){
+        return listaComentariosNormalizados;
+    }
+
+    public Vector<String> obtenerVectorPalabras(){
+        return vectorPalabras;
+    }
+
+    public Vector<VectorFrecuenciasPalabras> obtenerListaVectoresDeFrecuencias(){
+        return listaVectoresDeFrecuencias;
+    }
+}
+
