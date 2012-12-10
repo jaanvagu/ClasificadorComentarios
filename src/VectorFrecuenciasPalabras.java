9b03cf5f210a20514db2cf7c9ce598a88bc16960
Jairo Andrés Valencia <jaanvagu@gmail.com>
2012-12-10 13:12:06 -0500

    Clase VectorFrecuenciasPalabras añadida
    Clase encargada de estructurar un vector que almacena las frecuencias de
    las palabras en los comentarios.

---
+++ src/VectorFrecuenciasPalabras.java
@@ -0,0 +1,41 @@
+/*
+ * @author Jairo Andrés
+ */
+
+
+import java.util.*;
+
+public class VectorFrecuenciasPalabras {
+
+    private Vector<Integer> vectorFrecuenciasPalabras;
+
+    public VectorFrecuenciasPalabras(int tamanioVectorPalabras){
+        vectorFrecuenciasPalabras = new Vector(tamanioVectorPalabras);
+        for(int i=0; i<tamanioVectorPalabras; i++)
+            vectorFrecuenciasPalabras.addElement(0);        
+    }
+
+    public void aumentarFrecuenciaEnPosicion(int posicion){
+        int frecuenciaActual = vectorFrecuenciasPalabras.elementAt(posicion);        
+        int frecuenciaNueva = ++frecuenciaActual;        
+        vectorFrecuenciasPalabras.setElementAt(frecuenciaNueva, posicion);
+    }
+
+    public void aumentarFrecuenciaEnPosicionDeComentarioVacio(){
+        int frecuenciaActual = vectorFrecuenciasPalabras.elementAt(vectorFrecuenciasPalabras.size()-1);
+        int frecuenciaNueva = ++frecuenciaActual;
+        vectorFrecuenciasPalabras.setElementAt(frecuenciaNueva, vectorFrecuenciasPalabras.size()-1);
+    }
+
+    public int elementoEn(int i){
+        return vectorFrecuenciasPalabras.elementAt(i);
+    }
+
+    public int tamanio(){
+        return vectorFrecuenciasPalabras.size();
+    }
+
+    public Vector<Integer> obtenerVectorFrecuenciasPalabras(){
+        return vectorFrecuenciasPalabras;
+    }
+}
