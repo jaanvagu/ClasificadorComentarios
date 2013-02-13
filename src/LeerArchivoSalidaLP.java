66ca1e437e6ac7b2d90b51358293fd6ea195518d
Jairo Andrés Valencia <jaanvagu@gmail.com>
2013-02-13 17:04:19 -0500

    Clase LeerArchivoSalidaLP añadida
    Clase encargada de leer el archivo (label_prop_out) generado por la
    herramienta Label Propagation, y generar listas con las etiquetas
    correspondientes y con las propagas por el algoritmo.

---
+++ src/LeerArchivoSalidaLP.java
@@ -0,0 +1,92 @@
+/*
+ * @author Jairo Andrés
+ * Ultima modificacion: Febrero 13 de 2013
+ */
+
+
+import java.io.*;
+import java.util.*;
+
+public class LeerArchivoSalidaLP {
+
+    private Vector<String> listaEtiquetasCorrespondientes;
+    private Vector<String> listaEtiquetasPropagadasLP;
+
+    public LeerArchivoSalidaLP() {
+        listaEtiquetasCorrespondientes = new Vector();
+        listaEtiquetasPropagadasLP = new Vector();
+        leerLineasArchivo();
+    }
+
+    private void leerLineasArchivo(){
+        try {
+            FileInputStream fis = new FileInputStream(new GestionarArchivos().obtenerRutaArchivo2());
+            InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+            BufferedReader bufferlector = new BufferedReader(isr);
+            String lineaLeida;            
+            while ((lineaLeida = bufferlector.readLine()) != null){                                                 
+                construirListaEtiquetasCorrespondientes(lineaLeida);
+                construirListaEtiquetasPropagadasLP(lineaLeida);                
+            }
+        }
+        catch (Exception e){                            
+            System.out.println("Error en Leer archivo salida LP: "+e.getMessage());
+        }        
+    }
+
+    //Método que se encarga de cosntruir una lista con las etiquetas solución o correspondientes de una lista de comentarios.
+    // La lista es leída de un archivo generado por Label Propagation.
+    private void construirListaEtiquetasCorrespondientes(String lineaLeida){
+        StringTokenizer stLineaLeida = new StringTokenizer(lineaLeida);        
+        if(!esEtiquetaSemilla(lineaLeida)){            
+            StringBuilder sbEtiquetaLeida = new StringBuilder();
+            stLineaLeida.nextToken(); //Salto de un token que corresponde al identificador del nodo (Ni).
+            while(true){
+                String parte = stLineaLeida.nextToken();
+                if(parte.startsWith("1"))
+                    break;
+                sbEtiquetaLeida.append(parte).append(" ");
+            }
+            listaEtiquetasCorrespondientes.addElement(sbEtiquetaLeida.toString().trim());
+        }
+    }
+
+    //Método que se encarga de cosntruir una lista con las etiquetas que fueran puestas por el Label Propagation,
+    // La lista es leída de un archivo generado por Label Propagation.
+    private void construirListaEtiquetasPropagadasLP(String lineaLeida){
+        StringTokenizer stLineaLeida = new StringTokenizer(lineaLeida);
+        if(!esEtiquetaSemilla(lineaLeida)){
+            StringBuilder sbEtiquetaLeida = new StringBuilder();
+            while(!stLineaLeida.nextToken().startsWith("1")){}
+            while(true){
+                String parte = stLineaLeida.nextToken();
+                if(parte.startsWith("0"))
+                    break;
+                sbEtiquetaLeida.append(parte).append(" ");
+            }
+            listaEtiquetasPropagadasLP.addElement(sbEtiquetaLeida.toString().trim());
+        }
+    }
+
+    //Método que identifica si la etiqueta leida, corresponde a una de las etiquetas que fue pasada como semilla de entrenamiento a LP.
+    private boolean esEtiquetaSemilla(String linea){
+        StringTokenizer stLinea = new StringTokenizer(linea);
+        int cantTokens = stLinea.countTokens();
+        for(int i=0; i<cantTokens-2; i++)
+            stLinea.nextToken();
+        String indicador = stLinea.nextToken();        
+        if(indicador.equals("false")){
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public Vector<String> obtenerListaEtiquetasCorrespondientes(){
+        return listaEtiquetasCorrespondientes;
+    }
+
+    public Vector<String> obtenerListaEtiquetasPropagadasLP(){
+        return listaEtiquetasPropagadasLP;
+    }
+}
