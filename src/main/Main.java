dc3c6798a4b2115a00b1b7efeec9268777fb1c8d
Jairo Andrés Valencia <jaanvagu@gmail.com>
2013-04-18 21:42:37 -0500

    Clase Main actualizada

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,23 @@
+package main;
+
 /*
  * @author Jairo Andrés
- * Ultima modificacion: Septiembre 13 de 2013
+ * Ultima modificacion: Abril 16 de 2013
  */
 
-
+import LP.GestionarSemillasLP;
+import SVM.SVM;
+import entrada_salida.GestionarArchivos;
+import entrada_salida.LeerArchivoCSV;
+import entrada_salida.LeerArchivoSalidaLP;
+import estructuras.Comentario;
+import estructuras.ComentarioNormalizado;
+import evaluacion.GestionarIndicadores;
+import extraccion_caracteristicas.GestionarDistancias;
+import extraccion_caracteristicas.GestionarVectorPalabras;
 import java.util.Vector;
+import preprocesamiento.Lematizar;
+import preprocesamiento.Preprocesamiento;
 
 public class Main {
 
@@ -14,9 +27,10 @@ public class Main {
     private static GestionarArchivos gestionArchivos;
     private static GestionarVectorPalabras gestionVectorPalabras;
     private static GestionarDistancias gestionDistancias;
-    private static GestionarSemillas gestionSemillas;
+    private static GestionarSemillasLP gestionSemillasLP;
     private static LeerArchivoSalidaLP leerArchivoSalidaLP;
     private static GestionarIndicadores gestionIndicadores;
+    private static SVM SVM;
 
     //Variable que almacena los comentarios de la siguiente manera:
     //  Lista de Comentarios Normalizados = [ComentarioNormalizado(1), ComentarioNormalizado(2), ..., ComentarioNormalizado(N)  ]
@@ -37,41 +51,63 @@ public class Main {
             listaComentariosNormalizados.addElement(comentarioNormalizadoActual);
         }
         gestionArchivos = new GestionarArchivos();
-        gestionArchivos.guardarComentariosNormalizados(listaComentariosNormalizados);
+        gestionArchivos.guardarComentariosNormalizados(listaComentariosNormalizados,"Normalizado");
     }           
 
     
     public static void main(String[] args) {                
-        /*
-        leerArchivoCSV = new LeerArchivoCSV();
-        leerArchivoCSV.leerYAlmacenarLineasCSV();
-        preprocesamiento = new Preprocesamiento(leerArchivoCSV.obtenerListaComentariosLeidos());
-        lematizar = new Lematizar(preprocesamiento.obtenerMensajesProcesados());
-        normalizarComentarios(lematizar.obtenerListaMensajesLematizados(), leerArchivoCSV.obtenerListaComentariosLeidos());
-        //*/
+        
+        // ********* LECTURA Y PREPROCESAMIENTO *********        
+//        leerArchivoCSV = new LeerArchivoCSV();
+//        leerArchivoCSV.leerYAlmacenarLineasCSV();
+//        preprocesamiento = new Preprocesamiento(leerArchivoCSV.obtenerListaComentariosLeidos());
+//        lematizar = new Lematizar(preprocesamiento.obtenerMensajesProcesados());
+//        normalizarComentarios(lematizar.obtenerListaMensajesLematizados(), leerArchivoCSV.obtenerListaComentariosLeidos());                
 
 //        gestionArchivos = new GestionarArchivos();
-//        listaComentariosNormalizados = gestionArchivos.cargarComentariosNormalizados();
-//        DistribuirDatos distribuir = new DistribuirDatos(listaComentariosNormalizados);
-//        listaComentariosNormalizados = new Vector(distribuir.generarDistribucionNDatos(200));        
-//        System.out.println("Tam Com Distribuidos: "+listaComentariosNormalizados.size());
-//
-//        gestionVectorPalabras = new GestionarVectorPalabras();
-//        gestionDistancias = new GestionarDistancias();
-//        gestionSemillas = new GestionarSemillas(listaComentariosNormalizados);
+//        listaComentariosNormalizados = gestionArchivos.cargarComentariosNormalizados(2);
+
+        // ********* DISTRIBUCIÓN DE DATOS *********
+//        DistribuirDatos distribuir = new DistribuirDatos(listaComentariosNormalizados);  
+////        distribuir.generarListaSoloComentarioUtiles(); 
+//        listaComentariosNormalizados = new Vector(distribuir.generarDistribucionNDatos(400));        
+        
+        // ********* IMPRIMIR TEXTO DE LOS COMENTARIOS *********
+//        for(int i=0; i<listaComentariosNormalizados.size(); i++){
+//            System.out.println(listaComentariosNormalizados.elementAt(i).obtenerListaPalabrasEnComentario());
+//        }
+        
+        // ********* EXTRACCIÓN DE CARACTERÍSTICAS *********
+//        gestionVectorPalabras = new GestionarVectorPalabras();        
 //        gestionVectorPalabras.contruirVectorDePalabras();
 //        gestionVectorPalabras.generarVectoresDeFrecuenciasDePalabras();
+        
+        // ********* SVM *********
+//        SVM = new SVM(listaComentariosNormalizados, gestionVectorPalabras.obtenerListaVectoresDeFrecuencias());
+//        SVM.ejecutarSVM(20);     
+        
+        // ********* INDICADORES PARA SVM *********
+//        gestionIndicadores = new GestionarIndicadores(SVM.getTabla_Etiqueta_AparicionesTotalesEstimadas(),SVM.getTabla_Etiqueta_AparicionesCorrectas(), SVM.getTabla_Etiqueta_AparicionesTotalesCorrespondientes());
+//        gestionIndicadores.calcularIndicadores("SVM");
+        
+        // ********* LABEL PROPAGATION *********        
+//        gestionDistancias = new GestionarDistancias();        
 ////        gestionDistancias.calcularDistanciaEuclideanaEntreCadaParDeComentarios(gestionVectorPalabras.obtenerListaVectoresDeFrecuencias());
 ////        gestionDistancias.calcularDistanciaEuclideanaInvertidaEntreCadaParDeComentarios(gestionVectorPalabras.obtenerListaVectoresDeFrecuencias());
 //        gestionDistancias.calcularSimilitudCosenoEntreCadaParDeComentarios(gestionVectorPalabras.obtenerListaVectoresDeFrecuencias());
-//        gestionSemillas.generarArchivoSemillas(20);
-//        gestionSemillas.generarArchivoSolucion();
+//        gestionSemillasLP = new GestionarSemillasLP(listaComentariosNormalizados);
+//        gestionSemillasLP.generarArchivoSemillas(20);
+//        gestionSemillasLP.generarArchivoSolucion();
+//        ArchivoConfiguracionLP.generarArchivo(listaComentariosNormalizados.size());
+        
+        // ********* INDICADORES PARA LABEL PROPAGATION *********        
         leerArchivoSalidaLP = new LeerArchivoSalidaLP();
         gestionIndicadores = new GestionarIndicadores(leerArchivoSalidaLP.obtenerListaEtiquetasCorrespondientes(), leerArchivoSalidaLP.obtenerListaEtiquetasPropagadasLP());
-        gestionIndicadores.calcularIndicadorPresicionParaEtiquetas();
-        gestionIndicadores.calcularIndicadorRecallParaEtiquetas();
-        gestionIndicadores.calcularIndicadorFScoreParaEtiquetas();
-        gestionIndicadores.calcularAciertosTotales();
+        gestionIndicadores.calcularIndicadores("LP");
+                
+        System.setErr(null);
+        System.setOut(null);
+        System.exit(0);
     }
     
 }    
